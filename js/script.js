936831ce75f5e45b6efe40ea383fa4fc7b661533
MerantauWarrior <MerantauWarrior@users.noreply.github.com>
2019-06-01 20:09:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   // landing menu
   var allItems = document.getElementsByClassName('landing-menu__item');
-  for(var i = 0; i < allItems.length; i++){
+  for (var i = 0; i < allItems.length; i++) {
     allItems[i].classList.add('landing-menu__item_opened');
-    if(i>5 && i<allItems.length){
+    if (i > 5 && i < allItems.length) {
       allItems[i].classList.remove('landing-menu__item_opened');
     }
   }
   var landingToggle = document.getElementsByClassName('landing-menu__toggle')[0];
-  if(typeof(landingToggle) != 'undefined' && landingToggle != null){
+  if (typeof(landingToggle) != 'undefined' && landingToggle != null) {
     landingToggle.addEventListener('click', function () {
       this.classList.toggle('landing-menu__toggle_opened');
       var allItems = document.getElementsByClassName('landing-menu__item');
-      for(var i = 0; i < allItems.length; i++){
+      for (var i = 0; i < allItems.length; i++) {
         allItems[i].style.height = allItems[i].style.height === '63px' ? '44px' : '63px';
-        if(i>5 && i<allItems.length){
+        if (i > 5 && i < allItems.length) {
           allItems[i].classList.toggle('landing-menu__item_opened')
         }
       }
@@ -23,13 +23,26 @@ document.addEventListener("DOMContentLoaded", function () {
   //  main menu
   document.getElementsByClassName('navigation-menu-btn')[0].addEventListener('click', function () {
     this.classList.toggle('navigation-menu-btn_opened');
-    if(this.getAttribute('title') === 'Click to close'){
+    if (this.getAttribute('title') === 'Click to close') {
       this.setAttribute('title', 'Click to open');
-    }else {
+    } else {
       this.setAttribute('title', 'Click to close');
     }
     document.getElementsByClassName('navigation-menu')[0].classList.toggle('navigation-menu_opened');
   });
 
+  // modal windows
+  if (typeof( document.getElementsByClassName('modal-report-thanx__close')[0]) != 'undefined' &&  document.getElementsByClassName('modal-report-thanx__close')[0] != null) {
+    var closeModal = [].slice.call(document.getElementsByClassName('modal-box__close'));
+    closeModal.forEach(function (element, i) {
+      element.addEventListener('click', closeModalHandler);
+    });
+    document.getElementsByClassName('modal-report-thanx__close')[0].addEventListener('click', closeModalHandler);
+
+    function closeModalHandler(e) {
+      e.preventDefault();
+      document.getElementsByClassName('modal')[0].style.display = 'none';
+    }
+  }
 
 });
